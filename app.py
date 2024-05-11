571bee8e43c695d1115711c1d32819b9e7a886ac
sanju179 <sanjeevinir17@outlook.com>
2024-05-11 22:22:39 +0530

    full integration with 1 model

--- app.py
+++ app.py
@@ -1,20 +1,64 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 # from tensorflow.keras.models import load_model
 import numpy as np
 # from tensorflow.keras.preprocessing.image import load_img
 # from tensorflow.keras.applications.vgg16 import preprocess_input
 import os
 # from tensorflow.keras.preprocessing import image
+from PIL import Image
+from transformers import pipeline
+from pathlib import Path
+import uuid
+import pymongo
 
 app = Flask(__name__)
 # model = load_model('model.h5')
-
+pipe = pipeline("image-classification", "gianlab/swin-tiny-patch4-window7-224-finetuned-parkinson-classification")
 # target_img = os.path.join(os.getcwd() , 'static/images')
 
-@app.route('/')
-def index_view():
-    return render_template('main.html')
 
+# Connection string
+connection_string = "mongodb+srv://sem6:ssn@cluster0.q0hpe8v.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+
+# Connect to MongoDB
+client = pymongo.MongoClient(connection_string)
+
+# Access a specific database
+db = client['ndd_prediction']
+
+# Access a specific collection within the database
+collection = db['Credentials']
+
+for document in collection.find():
+    print(document)
+
+@app.route('/login', methods=['POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form.get('username')
+        password = request.form.get('password')
+        password=int(password)
+        
+        if username and password:
+            # Query MongoDB for the username and password
+            user = collection.find_one({"Username": username, "Password": password})
+            
+            if user:
+                # If user exists, render the home page template
+                print("sucess")
+                return redirect(url_for('index'))
+    
+    # If user does not exist or credentials are incorrect, redirect back to the login page
+    return render_template('login.html')
+
+@app.route('/index')  # Define the route URL path
+def index():
+    # You can render the index.html template here
+    return render_template('index.html')
+
+@app.route('/')
+def home():
+    return render_template('login.html')
 
 #Allow files with IMGension png, jpg and jpeg
 ALLOWED_IMG = set(['jpg' , 'jpeg' , 'png'])
@@ -44,7 +88,47 @@ def predict():
         acc = request.files['accelerometer']
         
         if (file and allowed_img(file.filename)) or (acc and allowed_file(acc.filename)): #Checking file format
-            return render_template('result.html',prob="100%",user_image="static/image_1.jpg", img_name=file.filename, acc_name=acc.filename)
+            image_path = Path('temp.png')
+            file.save(image_path)
+
+            # Open and resize the image
+            image = Image.open(image_path).resize((200, 200))
+
+            temp_image_filename = str(uuid.uuid4()) + '.jpg'
+            temp_image_path = os.path.join('static/temp_images', temp_image_filename)
+
+            # Save the resized image
+            image = image.convert('RGB')
+            image.save(temp_image_path)
+
+
+            # Make prediction using the model
+            predictions = pipe(image)
+
+            # Delete the temporary image file
+            image_path.unlink()
+
+            # Format the predictions
+            formatted_predictions = []
+            maxi = 0
+            maxi_label = ""
+            for prediction in predictions:
+                label = prediction['label']
+                score = prediction['score']
+                if score > maxi:
+                  maxi = score
+                  maxi_label = label
+                formatted_prediction = f"{label} => 'score': {score}"
+                formatted_predictions.append(formatted_prediction)
+
+
+            # Apply conditional formatting for 'parkinson' label
+            for i, prediction in enumerate(predictions):
+                if prediction['label'] == 'parkinson' and prediction['score'] > 0.5:
+                    formatted_predictions[i] = f"<span style='color:red'>{formatted_predictions[i]}</span>"
+
+            
+            return render_template('result.html',disease=maxi_label,prob=maxi,user_image=temp_image_path, img_name=file.filename, acc_name=acc.filename)
             """ filename = file.filename
             file_path = os.path.join('static/images', filename)
             file.save(file_path)
@@ -64,4 +148,5 @@ def predict():
 
 
 if __name__ == '__main__':
-    app.run(debug=True,use_reloader=False, port=8000)
+  
+    app.run(debug=True,use_reloader=True, port=8000)
